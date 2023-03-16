681f1e04c9d4a3ff8db00641ff6a459f730a00e9
Grigorii Skovorodnikov <gskovorod3@gmail.com>
2023-03-16 21:46:19 +0500

    add dz25

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N
+
+int ReadInt(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumNumbers(int m1, int n1)
+{
+    if (m1 == n1) return m1;
+    return m1 + SumNumbers(m1 + 1, n1);
+
+}
+
+int m = ReadInt("Input number M ");
+int n = ReadInt("Input number N ");
+if (m < n)
+{
+    System.Console.WriteLine(SumNumbers(m, n));
+}
+else
+{
+    System.Console.WriteLine(SumNumbers(n, m));
+}
